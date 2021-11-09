76f96f5a4bfc7a55adf2a6d6457570ecb3680a26
EC2 Default User <ec2-user@ip-172-31-3-254.ap-northeast-1.compute.internal>
2021-11-09 04:23:10 +0000

    [Start]投稿ページの作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,29 @@
 class PostsController < ApplicationController
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    @post.save
+    redirect_to root_path
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :contents, :image)
+  end
+
 end
