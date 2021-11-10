19db150cb4175376556dd2278bb6afe3a02f057e
EC2 Default User <ec2-user@ip-172-31-3-254.ap-northeast-1.compute.internal>
2021-11-10 01:51:54 +0000

    [Add]投稿ページの作成

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '412fbf6f70bd298104eef6272b3de0d89ef10a547f5b8e7e35f94d06f4792796b9d2310dabd331b8713d37cda431da67791fd1be3682225706f8615eb9fa2ee5'
