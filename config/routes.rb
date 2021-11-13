019a104f7d4ee92e36ec456888db1267736ffd20
EC2 Default User <ec2-user@ip-172-31-3-254.ap-northeast-1.compute.internal>
2021-11-13 00:06:04 +0000

    [Finish]comment機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'posts#index'
-  resources :posts, only: [:new, :create, :show, :destroy]
+  resources :posts, only: [:new, :create, :show, :destroy] do
+    resources :comments, only: [:create, :destroy]
+  end
+  
 
 end
