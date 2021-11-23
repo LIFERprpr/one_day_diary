b28a97043112ee7f150e40d153970601ad3b996c
EC2 Default User <ec2-user@ip-172-31-3-254.ap-northeast-1.compute.internal>
2021-11-23 05:44:29 +0000

    [Finish]バリデーションの設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,10 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :contents, presence: true, length: { maximum: 200 }
+
+
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
